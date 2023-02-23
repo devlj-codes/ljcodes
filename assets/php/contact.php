b78bb5030d0aeadff288dd96b003865318dd7357
devlj-codes <lj.molinacaoile@gmail.com>
2023-02-23 13:07:35 +0800

    Updated contact.php

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,25 +1,11 @@
 <?php
-// Get data from form 
 $name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
- 
-$to = "laurencejoy.abuzo@gmail.com";
-$subject = "LEADS - LJ Codes";
- 
-// The following text will be sent
-// Name = user entered name
-// Email = user entered email
-// Message = user entered message
-$txt ="Name = ". $name . "\r\n  Email = "
-    . $email . "\r\n Message =" . $message;
- 
-$headers = "From: noreply@ljcodes.com" . "\r\n" .
-            "CC: laurencejoy.abuzo@gmail.com";
-if($email != NULL) {
-    mail($to, $subject, $txt, $headers);
-}
- 
-// Redirect to
-header("Location:index.html");
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "laurencejoy.abuzo@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
